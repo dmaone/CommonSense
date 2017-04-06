112bc96255a8156873e82664e15d2b3468e33952
DMA <dma@ya.ru>
2017-04-06 12:48:02 -0700

    Saner filenames for icons

--- Qt-build/mac-prebuild.sh
+++ Qt-build/mac-prebuild.sh
@@ -5,9 +5,11 @@ pushd ../FlightController-icons
 mkdir FlightController.iconset
 cd FlightController.iconset
 cp -a ../icon_* .
-cp -f icon_32x32.png icon_16x16@2x.png
+cp -f icon_1024x1024.png icon_512x512@2x.png
 cp -f icon_512x512.png icon_256x256@2x.png
 cp -f icon_256x256.png icon_128x128@2x.png
+cp -f icon_64x64.png icon_32x32@2x.png
+cp -f icon_32x32.png icon_16x16@2x.png
 cd ..
 iconutil -c icns FlightController.iconset
 cp -a FlightController.icns ../FlightController
